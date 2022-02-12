a80e4be2acb4a3584cdae1b3f51630b8fc76d823
Victor Costa <vcosta@nelogica.com.br>
2022-02-12 18:10:44 -0300

    Added logs and updated README.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # LostArkAutoFishing
+
+Auto fishing bot for https://www.playlostark.com
+
+## Instalation
+`>>> pip install -r requirements.txt`
+
+## Runing
+`>>> python main.py`
+
+Open the game and use the fishing skill once, then the bot takes over.
+
+## Notes
+
+- Keep your mouse over water while its running;
+- Works in FHD and UWFHD resolutions;
--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import time
 
 
 def fish():
+    print("LostArkAutoFishing STARTED, open the game and start fishing.")
+
     while 1:
         if pyautogui.locateOnScreen('exclamation_mark.png', confidence=0.8) is not None:
-            print("ACHEI")
+            print("Fish caught")
             pyautogui.press('e')
 
             time.sleep(7)
 
-            print("Pescando novamente")
+            print("Fishing started")
             pyautogui.press('e')
 
         time.sleep(0.2)
